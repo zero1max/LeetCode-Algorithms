601c17a8098e6ef82fe05b0bcf6bd47d422b32c2
zero1max <nigmatjonovmuhammad08@gmail.com>
2025-01-17 11:30:47 +0500

    Update Commit

---
+++ Longest Common Prefix(14)/main.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+        print(prefix)
+        
+        for string in strs[1:]:
+            while string[:len(prefix)] != prefix and prefix:
+                prefix = prefix[:-1] 
+            if not prefix:
+                break
+        
+        return prefix
+    
+
+
